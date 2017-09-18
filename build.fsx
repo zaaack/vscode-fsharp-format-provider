0d21848e9ae7a22e5a5e548d8d8ee973c34df92d
zaaack <zaaack@qq.com>
2017-09-18 22:53:56 +0800

    Fix deps

--- build.fsx
+++ build.fsx
@@ -1,11 +1,3 @@
-#r "/Users/z/.nuget/packages/Paket.Core/5.96.2/lib/netstandard1.6/Paket.Core.dll" 
-
-#r "/Users/z/.nuget/packages/System.IO.FileSystem.Primitives/4.3.0/lib/netstandard1.3/System.IO.FileSystem.Primitives.dll" 
-
-#r "/Users/z/.nuget/packages/System.IO.FileSystem/4.3.0/lib/net46/System.IO.FileSystem.dll"
-open Paket
 
 
-let file = Paket.DependenciesFile.FindLocalfile "FantomasCLI"
-
-printfn "%A" file
+let fantomasCLIPath = "paket-files/github.com/dungpa/fantomas/src/Fantomas.Cmd/bin/Release"
